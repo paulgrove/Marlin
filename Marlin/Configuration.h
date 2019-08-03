1681f2e8a54ba308ca14fe8d38bc3dbc3bd93f6c
Paul Grove <paul.a.grove@gmail.com>
2019-08-03 23:59:57 +0100

    Correction to build dimentions

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1050,14 +1050,14 @@
 
 // The size of the print bed
 #define X_BED_SIZE 250
-#define Y_BED_SIZE 280
+#define Y_BED_SIZE 250
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
-#define X_MIN_POS 10
-#define Y_MIN_POS 10
+#define X_MIN_POS -10
+#define Y_MIN_POS -25
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE + 40
-#define Y_MAX_POS Y_BED_SIZE + 10
+#define Y_MAX_POS Y_BED_SIZE + 25
 #define Z_MAX_POS 300
 
 /**

ba068f021dbaa2da8c952e933fed67567159a229
Paul Grove <paul.a.grove@gmail.com>
2019-08-03 03:17:28 +0100

    Not a float

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -916,7 +916,7 @@
  *     O-- FRONT --+
  *   (0,0)
  */
-#define X_PROBE_OFFSET_FROM_EXTRUDER -43.5  // X offset: -left  +right  [of the nozzle]
+#define X_PROBE_OFFSET_FROM_EXTRUDER -43  // X offset: -left  +right  [of the nozzle]
 #define Y_PROBE_OFFSET_FROM_EXTRUDER -5  // Y offset: -front +behind [the nozzle]
 #define Z_PROBE_OFFSET_FROM_EXTRUDER 0   // Z offset: -below +above  [the nozzle]
 
